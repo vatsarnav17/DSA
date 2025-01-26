b0cbd1af96222520a76d7a840ea8d2b873d93415
vatsarnav17 <vatsarnav17@gmail.com>
2025-01-26 17:01:40 +0530

    26-01-1

---
+++ January/cookie.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& greed, vector<int>& cookieSize) {
+    int n = greed.size();
+    int m = cookieSize.size();
+    sort(greed.begin(), greed.end());
+    sort(cookieSize.begin(), cookieSize.end());
+    int l = 0;
+    int r = 0;
+    while (l < m && r < n) {
+        if (greed[r] <= cookieSize[l]) {
+            r++;
+        }
+        l++;
+    }
+    return r;
+}
+    
+};
