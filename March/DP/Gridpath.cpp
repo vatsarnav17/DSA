0b6a58750b56c37a6fdb7db6d77fda60f570d313
vatsarnav17 <vatsarnav17@gmail.com>
2025-03-04 12:16:24 +0530

    04-01

---
+++ March/DP/Gridpath.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        int uniquePaths(int m, int n) {
+            vector<int> arr(n, 1);
+                 
+            for(int i=1; i<m; ++i) {
+                for(int j=1; j<n; ++j) {
+                    arr[j] += arr[j-1];
+                }
+            }
+    
+            return arr[n-1];
+        }
+    };
