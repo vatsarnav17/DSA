9843dc5b7a5d30d7dc3bc98462134a323c5d3cef
vatsarnav17 <vatsarnav17@gmail.com>
2025-04-04 13:34:46 +0530

    04-01

--- April/DP/allpath.cpp
+++ April/DP/allpath.cpp
@@ -31,3 +31,24 @@ class Solution {
 
 //TABULATION
 
+class Solution {
+    public:
+        int uniquePaths(int m, int n) {
+            vector<vector<int>> dp(m, vector<int>(n, 0));
+    
+            // Base case: There's only one way to reach the starting point
+            dp[0][0] = 1;
+    
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == 0 && j == 0) continue; // Skip the starting cell
+    
+                    int up = (i > 0) ? dp[i - 1][j] : 0;
+                    int left = (j > 0) ? dp[i][j - 1] : 0;
+    
+                    dp[i][j] = up + left;
+                }
+            }
+            return dp[m - 1][n - 1];
+        }
+    };
