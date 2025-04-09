6731fe7078781c145efda19767e19aadef8a4212
vatsarnav17 <vatsarnav17@gmail.com>
2025-04-09 13:02:11 +0530

    09-02

--- April/DP/Triangle.cpp
+++ April/DP/Triangle.cpp
@@ -30,3 +30,30 @@ class Solution {
 
 //TABULATION
 
+class Solution {
+    public:
+        int minimumTotal(vector<vector<int>>& triangle) {
+            int m = triangle.size();
+            vector<vector<int>> dp;
+            for (int i = 0; i < m; i++) {
+                dp.push_back(vector<int>(triangle[i].size(), 0));
+            }
+    
+            // Initialize last row
+            for (int j = 0; j < triangle[m - 1].size(); j++) {
+                dp[m - 1][j] = triangle[m - 1][j];
+            }
+    
+            // Bottom-up iteration
+            for (int i = m - 2; i >= 0; i--) {
+                for (int j = 0; j < triangle[i].size(); j++) {
+                    int d = dp[i + 1][j];
+                    int dg = dp[i + 1][j + 1];
+                    dp[i][j] = triangle[i][j] + min(d, dg);
+                }
+            }
+    
+            return dp[0][0];
+        }
+    };
+    
