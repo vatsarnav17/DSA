8541b7319cf064cdf3575db163b6199a2a358919
vatsarnav17 <vatsarnav17@gmail.com>
2025-04-11 12:57:06 +0530

    11-01

--- April/DP/minpathfall.cpp
+++ April/DP/minpathfall.cpp
@@ -1,3 +1,5 @@
+//MEMOIZATION 
+
 class Solution {
     public:
         int f(int row,int idx,vector<vector<int>>&mat,vector<vector<int>>&dp)
@@ -32,3 +34,38 @@ class Solution {
             return res;
         }
     };
+
+    //TABULATION
+
+    class Solution {
+        public:
+            int minFallingPathSum(vector<vector<int>>& matrix) {
+                int n = matrix.size();
+                vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+                // Base case: last row is the same as matrix
+                for (int col = 0; col < n; ++col) {
+                    dp[n - 1][col] = matrix[n - 1][col];
+                }
+        
+                // Bottom-up DP
+                for (int row = n - 2; row >= 0; --row) {
+                    for (int col = 0; col < n; ++col) {
+                        int down = dp[row + 1][col];
+                        int leftDiag = (col > 0) ? dp[row + 1][col - 1] : 1e9;
+                        int rightDiag = (col < n - 1) ? dp[row + 1][col + 1] : 1e9;
+        
+                        dp[row][col] = matrix[row][col] + min({down, leftDiag, rightDiag});
+                    }
+                }
+        
+                // Find the minimum in the first row
+                int res = 1e9;
+                for (int col = 0; col < n; ++col) {
+                    res = min(res, dp[0][col]);
+                }
+        
+                return res;
+            }
+        };
+        
